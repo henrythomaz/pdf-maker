ec59e13e497e7c39f46813a5f5e44230b3423a34
Henry Campos <henry.campos_estudan@C135757.pdc-academico.ifms.tl>
2025-07-25 17:19:24 -0400

    Criando a funcionalidade de criar o arquivo json

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,16 @@
+import fs from "fs"
+import path from "path"
 import app from "./app.js";
 import dotenv from "dotenv";
+
+const contentPath = path.join(__dirname, 'content.json');
+
+if (!fs.existsSync(contentPath)) {
+  fs.writeFileSync(contentPath, JSON.stringify({}, null, 2), 'utf-8');
+  console.log('Arquivo content.json criado com sucesso!');
+} else {
+  console.log('Arquivo content.json já existe.');
+}
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
